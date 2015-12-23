6bc6e0af10dac170f241366a3300164e2f4a5bba
Benjamin Barnes <benlukebarnes@gmail.com>
2015-12-23 15:18:08 -0800

    q

--- visual.php
+++ visual.php
@@ -347,11 +347,8 @@ function showOverlayOptions($getPage_connection2) {
 	if ($stmt = $getPage_connection2->prepare("SELECT id,name FROM overlays ORDER BY id ASC")) {
 		$stmt->execute();
 		$stmt->bind_result($r_id,$r_name);
-		$stmt->fetch();
 		
-		while ($stmt->fetch()) {
-			//$overlayInfo = getOverlayInfo($getPage_connection2,$r_result);
-			
+		while ($stmt->fetch()) {			
 			if (isset($_SESSION["overlay"])) {
 				if ($_SESSION["overlay"] == strtolower($r_name)) {
 					echo "            <option class=\"option1\" selected=\"selected\" value=\"".strtolower($r_name)."\">".$r_name."</option>\n";
