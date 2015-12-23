03897c9a388e2b91a565dc17bc9651cbe0e47cae
Benjamin Barnes <benlukebarnes@gmail.com>
2015-12-23 13:54:50 -0800

    q
    qqq

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+//getGlobals_organizations($getPage_connection2);
+//showTitle("Organizations");
+compileMenu($getPage_connection2,"organizations");
+//showWarning($getPage_connection2);
+//showOrganizationsInfo($getPage_connection2);
+require "section4.txt";
+//resetSession(true);
+?>
