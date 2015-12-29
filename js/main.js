233f03ead734bc81731bf05117188388e613d38b
Benjamin Barnes <benlukebarnes@gmail.com>
2015-12-28 16:22:17 -0800

    q

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 /****************************************************************************
 * Name:        main.js
 * Author:      Ben Barnes
-* Date:        2015-12-21
+* Date:        2015-12-28
 * Purpose:     Client-side error checking and interface enhancing.
 *****************************************************************************/
 /* Main Body Loading */
 function bodyLoad() {
+	$(function () {
+		  $('[data-toggle="tooltip"]').tooltip()
+		})
 } // bodyLoad
